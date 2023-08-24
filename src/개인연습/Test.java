7d6d1d80ea637e73958abe54b5161c1be9b51e63
11yhj <uouo_@naver.com>
2023-08-24 19:46:37 +0900

    Java

---
+++ src/개인연습/Test.java
@@ -0,0 +1,91 @@
+package 개인연습;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+
+
+
+//조건문 if와 반복문 for
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("별나무 줄 숫자를 입력 하세요. : ");
+//        double num = sc.nextDouble();
+//
+//        String star = "";
+//
+//        for (int num1= 1; num1 <= num; num1++) {
+//            for (int count = 1; count <= num1; count++) {
+//                star += "*";
+//            }
+//            if (num1 <= num-1) {
+//                star += "\n";
+//            }
+//            System.out.println(star);
+//        }
+
+
+
+
+
+//반복문 for 안의 for
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("별나무 줄 숫자를 입력 하세요. : ");
+//        double num = sc.nextDouble();
+//
+//        String star = "";
+//        for (int num1= 1; num1 <= num; num1++){
+//            for (int count = 1; count <= num1; count++){
+//                star += "*";
+//            }
+//            star += "\n";
+//        }
+//        System.out.println(star);
+
+
+
+
+// 반복문 for
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("식권이 몇 장 필요한가요? :");
+//        double some = sc.nextDouble();
+//
+//        for (int chul = 1; chul <=some; chul = chul+1)
+//        {
+//            System.out.println("[식권]" + chul);
+//        }
+
+
+
+
+// else if 연습
+
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("피자 가게가 열렸나요(y/n)? ");
+//        char pizza = sc.next().charAt(0);
+//        System.out.print("치킨 가게가 열렸나요(y/n)? ");
+//        char chik = sc.next().charAt(0);
+//        System.out.print("햄버거 가게가 열렸나요(y/n)?");
+//        char ham = sc.next().charAt(0);
+//        System.out.print("우동 가게가 열렸나요(y/n)");
+//        char udonh = sc.next().charAt(0);
+//
+//
+//        if (pizza == 'y'){
+//            System.out.print("피자 가게로 가자");
+//        }else if(chik == 'y') {
+//            System.out.print("치킨 가게로 가자");
+//        } else if (ham == 'y') {
+//            System.out.print("햄버거 가게로 가자");
+//        } else if (udonh == 'y') {
+//            System.out.print("우동 가게로 가자");
+//
+//    }else{
+//            System.out.print("편의점에서 라면 먹자");
+//        }
+    }
+}
